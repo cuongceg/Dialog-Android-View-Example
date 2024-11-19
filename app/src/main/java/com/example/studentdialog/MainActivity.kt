62790047a0af5098398929d0a934f5daafd08760
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-19 11:44:08 +0700

    feat : add student RecycleView

--- app/src/main/java/com/example/studentdialog/MainActivity.kt
+++ app/src/main/java/com/example/studentdialog/MainActivity.kt
@@ -1,112 +1,104 @@
 package com.example.studentdialog
 
-import android.app.TimePickerDialog
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.TimePicker
+import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studentdialog.adapter.StudentAdapter
+import com.example.studentdialog.model.Student
+import com.example.studentdialog.viewmodel.StudentViewModel
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private val studentViewModel: StudentViewModel by viewModels()
+    private lateinit var studentAdapter: StudentAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button_add).setOnClickListener {
-//      AlertDialog.Builder(this)
-//        .setIcon(R.mipmap.ic_launcher_round)
-//        .setTitle("Test dialog")
-//        .setMessage("This is a test dialog")
-//        .setPositiveButton("Yes"
-//        ) { _, _ -> Log.v("TAG", "Yes is clicked") }
-//          .setNegativeButton("No"
-//        ) { _, _ -> Log.v("TAG", "No is clicked") }
-//          .setNeutralButton("Cancel", null)
-//        .show()
-//        .setCanceledOnTouchOutside(false)
-//
-//            val items = mutableListOf<String>()
-//            for (i in 1..50)
-//                items.add("Item $i")
 
-//      AlertDialog.Builder(this)
-//        .setTitle("Select an option")
-//        .setItems(items.toTypedArray(),
-//          { _, position -> Log.v("TAG", "$position")})
-//        .setPositiveButton("Cancel", null)
-//        .show()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        studentAdapter = StudentAdapter(
+            students = listOf(),
+            onEditClick = { student -> showEditDialog(student) },
+            onDeleteClick = { student -> showDeleteDialog(student) }
+        )
 
-//      AlertDialog.Builder(this)
-//        .setTitle("Select an option")
-//        .setSingleChoiceItems(items.toTypedArray(), 0,
-//          { _, position: Int -> Log.v("TAG", "$position")})
-//        .setPositiveButton("OK", { _, _ -> })
-//        .setNegativeButton("Cancel", null)
-//        .show()
+        recyclerView.adapter = studentAdapter
 
-//      val selections = BooleanArray(items.size)
-//
-//      AlertDialog.Builder(this)
-//        .setTitle("Select an option")
-//        .setMultiChoiceItems(items.toTypedArray(), selections,
-//          { _, pos: Int, isSelected: Boolean -> Log.v("TAG", "$pos: $isSelected")})
-//        .setPositiveButton("OK", { _, _ -> })
-//        .setNegativeButton("Cancel", null)
-//        .show()
-
-
-//      val dialogView = LayoutInflater.from(this)
-//        .inflate(R.layout.layout_alert_dialog, null)
-//
-//      val editHoten = dialogView.findViewById<EditText>(R.id.edit_hoten)
-//      val editMssv = dialogView.findViewById<EditText>(R.id.edit_mssv)
-//
-//      AlertDialog.Builder(this)
-//        .setTitle("Nhap thong tin sinh vien")
-//        .setView(dialogView)
-//        .setPositiveButton("OK", { _, _ ->
-//          val hoten = editHoten.text.toString()
-//          val mssv = editMssv.text.toString()
-//          Log.v("TAG", "$hoten - $mssv")
-//        })
-//        .setNegativeButton("Cancel", null)
-//        .show()
-
-
-//      val dialog = Dialog(this)
-//      dialog.setContentView(R.layout.layout_dialog)
-//      val editHoten = dialog.findViewById<EditText>(R.id.edit_hoten)
-//      val editMssv = dialog.findViewById<EditText>(R.id.edit_mssv)
-//      dialog.findViewById<Button>(R.id.button_ok).setOnClickListener {
-//        val hoten = editHoten.text.toString()
-//        val mssv = editMssv.text.toString()
-//        Log.v("TAG", "$hoten - $mssv")
-//        dialog.dismiss()
-//      }
-//      dialog.findViewById<Button>(R.id.button_cancel).setOnClickListener {
-//        dialog.dismiss()
-//      }
-//      dialog.window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-//      dialog.show()
-
-      DialogFragment().show(supportFragmentManager, "TAG")
-
-//      DatePickerDialog(this,
-//        { _, year: Int, month: Int, day: Int -> Log.v("TAG", "$year - $month - $day")},
-//        2024, 11, 14).show()
-
-//      TimePickerDialog(this,
-//        { _, hour, minute -> Log.v("TAG", "$hour:$minute")},
-//        10, 59, true).show()
-
-//      Toast.makeText(this, "Hello Android", Toast.LENGTH_LONG)
-//        .show()
-
-//            Snackbar.make(it, "Hello Android!", Snackbar.LENGTH_LONG)
-//                .setAction("UNDO") {}
-//                .show()
+        studentViewModel.students.observe(this){ students ->
+            studentAdapter.updateData(students)
+        }
+        findViewById<Button>(R.id.extended_fab).setOnClickListener {
+            showAddDialog()
         }
     }
+
+    private fun showAddDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.layout_dialog, null)
+        val etName = dialogView.findViewById<EditText>(R.id.edit_name)
+        val etId = dialogView.findViewById<EditText>(R.id.edit_id)
+
+        AlertDialog.Builder(this)
+            .setTitle("Add Student")
+            .setView(dialogView)
+            .setPositiveButton("Add") { _, _ ->
+                val name = etName.text.toString()
+                val id = etId.text.toString().toIntOrNull()
+                if (name.isNotBlank() && id != null) {
+                    if(studentViewModel.students.value?.find { it.id == id } == null){
+                        studentViewModel.addStudent(Student(id, name))
+                    }
+                    else{
+                        Toast.makeText(this, "Student ID already exists", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun showEditDialog(student: Student) {
+        val dialogView = layoutInflater.inflate(R.layout.layout_dialog, null)
+        val etName = dialogView.findViewById<EditText>(R.id.edit_name)
+        val etId = dialogView.findViewById<EditText>(R.id.edit_id)
+        etName.setText(student.name)
+        etId.setText(student.id.toString())
+
+        AlertDialog.Builder(this)
+            .setTitle("Edit Student")
+            .setView(dialogView)
+            .setPositiveButton("Update") { _, _ ->
+                val newName = etName.text.toString()
+                val newId = etId.text.toString().toIntOrNull()
+                if (newName.isNotBlank() && newId != null) {
+                    studentViewModel.updateInformationStudent(student.copy(name = newName, id = newId))
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun showDeleteDialog(student: Student) {
+        AlertDialog.Builder(this)
+            .setMessage("Do you want to delete student ${student.name}?")
+            .setPositiveButton("Delete") { _, _ ->
+                studentViewModel.deleteStudent(student)
+                Snackbar.make(findViewById(R.id.main), "${student.name} deleted", Snackbar.LENGTH_LONG)
+                    .setAction("Undo") {
+                        studentViewModel.addStudent(student)
+                    }.show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+
 }
